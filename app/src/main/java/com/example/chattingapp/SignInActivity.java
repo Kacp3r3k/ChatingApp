14a5042b9d15e1ff7080c3b8f39a501760590ed5
Kacp3r3k <kacperek.drozynski@gmail.com>
2022-04-03 12:06:12 +0200

    Commit3

--- app/src/main/java/com/example/chattingapp/SignInActivity.java
+++ app/src/main/java/com/example/chattingapp/SignInActivity.java
@@ -7,23 +7,34 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.chattingapp.Models.Users;
 import com.example.chattingapp.databinding.ActivitySignInBinding;
 import com.example.chattingapp.databinding.ActivitySignUpBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignInActivity extends AppCompatActivity {
     ActivitySignInBinding binding;
     ProgressDialog progressDialog;
     FirebaseAuth auth;
-
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +45,15 @@ public class SignInActivity extends AppCompatActivity {
         getSupportActionBar().hide();;
 
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
         progressDialog = new ProgressDialog(SignInActivity.this);
         progressDialog.setTitle("Login");
         progressDialog.setMessage("Login to your account");
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
 
 
 
@@ -68,9 +85,66 @@ public class SignInActivity extends AppCompatActivity {
 
             }
         });
+        binding.btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
         if (auth.getCurrentUser()!= null){
             Intent intent = new Intent(SignInActivity.this,MainActivity.class);
             startActivity(intent);
         }
+    }
+    int RC_SIGN_IN = 65;
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d("TAG","firebaseAuthWithGoogle:"+ account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            }catch(ApiException e){
+                Log.w("TAG","Google sign in failed",e);
+
+            }
+        }
+    }
+    private void  firebaseAuthWithGoogle(String idToken){
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken,null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Log.d("TAG","signInWithCredential:succes");
+                            FirebaseUser user = auth.getCurrentUser();
+                            Users users = new Users();
+                            users.setUserId(user.getUid());
+                            users.setUserName(user.getDisplayName());
+                            users.setProfilepic((user.getPhotoUrl().toString()));
+                            database.getReference().child("Users").child(user.getUid()).setValue(users);
+
+
+                           Intent intent =new Intent(SignInActivity.this,MainActivity.class);
+                           startActivity(intent);
+                            Toast.makeText(SignInActivity.this, "Sign in with Google", Toast.LENGTH_SHORT).show();
+
+
+                        }else{
+                            Log.w("TAG","signInWithCredential:Failiure", task.getException());
+                            Toast.makeText(SignInActivity.this,task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            Snackbar.make(binding.getRoot(),"A",Snackbar.LENGTH_SHORT);
+
+                        }
+                    }
+                });
     }
 }
